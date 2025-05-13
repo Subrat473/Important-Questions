342500b53b7c6b5e93da398001012afa15535a8d
Subrat <85737468+Subrat473@users.noreply.github.com>
2025-05-13 17:23:10 +0000

    Hoisting.js

---
+++ Important topics from js/Hoisting.js
@@ -0,0 +1,10 @@
+getName(); 
+console.log(x);
+var x = 4;
+function getName() {
+    console.log("namaste javascript");
+    
+}
+
+
+// Hoisting is a concept which enables us to ectract values of variables and functions even before initialising/assigning values without getting error and this is happening due to the 1st phase(memory creatiobn phase) of the execution of the Execution Context.
